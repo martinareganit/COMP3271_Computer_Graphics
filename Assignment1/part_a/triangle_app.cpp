1087769fb1408c35caf7c640a4ad0f2994c1eb9b
Martina Reganit <reganit@connect.hku.hk>
2026-09-25 18:29:15 +0800

    Implement interactive triangle drawing

--- Assignment1/part_a/triangle_app.cpp
+++ Assignment1/part_a/triangle_app.cpp
@@ -46,11 +46,47 @@ void DrawTriangles() {
 
     glColor3d(1.0, 1.0, 1.0);
     glPointSize(5.0F);
+    
     // ===== STUDENT_TASK_BEGIN: part_a_draw_triangles =====
+    
+    glBegin(GL_TRIANGLES);
+    for (int i = 0; i < triangles.size(); i++) {
+        int color_idx = triangles[i].color_index;
+        glColor3d(color_array[color_idx][0], color_array[color_idx][1], color_array[color_idx][2]);
+
+        glVertex2d(triangles[i].vertices[0][0], triangles[i].vertices[0][1]);
+        glVertex2d(triangles[i].vertices[1][0], triangles[i].vertices[1][1]);
+        glVertex2d(triangles[i].vertices[2][0], triangles[i].vertices[2][1]);
+    }
+    glEnd();
+
+    glColor3d(1.0, 1.0, 1.0);
+    glBegin(GL_POINTS);
+    for (int i = 0; i < point_count; i++) {
+        glVertex2d(triangle_to_draw.vertices[i][0], triangle_to_draw.vertices[i][1]);
+    }
+    glEnd();
+
     // ===== STUDENT_TASK_END: part_a_draw_triangles =====
 }
 
 void MouseInteraction(double m_x, double m_y) {
+    
     // ===== STUDENT_TASK_BEGIN: part_a_mouse_interaction =====
+    
+    triangle_to_draw.vertices[point_count][0] = m_x;
+    triangle_to_draw.vertices[point_count][1] = m_y;
+    point_count++;
+
+    if (point_count == 3) {
+        int total_triangles = (int)triangles.size();
+        int total_colors = (int)color_array.size();
+
+        triangle_to_draw.color_index = total_triangles % total_colors;
+        triangles.push_back(triangle_to_draw);
+
+        point_count = 0;
+    }
+
     // ===== STUDENT_TASK_END: part_a_mouse_interaction =====
 }
